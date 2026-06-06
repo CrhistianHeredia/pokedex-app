ef9f34cee6fd0db98f7fa84647c09d222d40f463
Crhistian Heredia <crhist.heredia@outlook.es>
2026-06-06 16:22:13 -0500

    feat: navbar redisenado con buscador centrado y estilo pill

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,31 +1,24 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import {Location} from '@angular/common';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  @Output() search = new EventEmitter;
+export class HeaderComponent {
+  @Output() search = new EventEmitter<string>();
   @Input() pageHome = true;
 
-  searchInput: string = "";
-  showFiller = false;
+  searchInput = '';
 
-  constructor(
-    private location: Location
-  ) { }
+  constructor(private location: Location) {}
 
-  ngOnInit(): void { }
-
-  searchPokemon(){
+  searchPokemon(): void {
     this.search.emit(this.searchInput);
   }
 
-  backClicked() {
+  backClicked(): void {
     this.location.back();
   }
 }
